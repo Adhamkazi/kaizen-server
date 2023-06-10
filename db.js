8bc1d8416b686f0cb235aa99db40bff746e25bcb
Adham kazi <princeadham67@gmail.com>
2023-06-11 05:08:15 +0530

    Changes

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
 const connection = mongoose.connect(process.env.MONGODB_URL);
-
+require('dotenv').config();
 module.exports = {connection};
