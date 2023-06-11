5cb9f4dcca02f1910ccabe157a1034120eea4ee9
Adham kazi <princeadham67@gmail.com>
2023-06-12 02:06:41 +0530

    Check

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+console.log(process.env.mongoURL);
 const connection = mongoose.connect(process.env.mongoURL);
 // const connection = mongoose.connect(process.env.mongoURL, {
 //     useNewUrlParser: true,
