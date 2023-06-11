fa12e183e3fb63397fa7d723be0a6fb821b14930
Adham kazi <princeadham67@gmail.com>
2023-06-12 01:56:37 +0530

    Check

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect(process.env.mongoURL);
-// const connection =  mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-//   });
+// const connection = mongoose.connect(process.env.mongoURL);
+const connection = mongoose.connect(process.env.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
 
 //   mongoose.Promise = global.Promise;
 module.exports = {connection};
