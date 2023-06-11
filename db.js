03c4ddb3f1fb7c6db70b0d33a89abd7d813b5e48
Adham kazi <princeadham67@gmail.com>
2023-06-12 02:11:17 +0530

    Check

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
-console.log(process.env.mongoURL);
-const connection = mongoose.connect(process.env.mongoURL);
+// console.log(process.env.mongoURL);
+const connection = mongoose.connect("mongodb+srv://adham:adham@cluster0.iq2zpvd.mongodb.net/kaizen?retryWrites=true&w=majority");
 // const connection = mongoose.connect(process.env.mongoURL, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
